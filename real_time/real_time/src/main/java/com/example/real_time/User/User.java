7f49e3fd7f457e9d734cbe9de72aa6bdc1ac3a57
yassine <yassine@mail.com>
2024-10-28 15:54:02 +0100

    initial_push

---
+++ real_time/real_time/src/main/java/com/example/real_time/User/User.java
@@ -0,0 +1,72 @@
+package com.example.real_time.User;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.security.Principal;
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "_user")
+public class User implements Principal, UserDetails {
+    @Id
+    private Integer id;
+    @Column(unique = true)
+    private String email;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private boolean enabled;
+    private boolean locked;
+
+    public String fullName() {
+        return firstName + " " + lastName;
+    }
+
+    @Override
+    public String getName() {
+        return email;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+}
